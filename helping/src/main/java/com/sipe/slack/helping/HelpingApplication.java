0fb5d232cda813ad8d37b3da742ec05002b96a2c
devifancy <fancy.junyongmoon@gmail.com>
2025-01-17 20:22:40 +0900

    init: project setting

---
+++ helping/src/main/java/com/sipe/slack/helping/HelpingApplication.java
@@ -0,0 +1,13 @@
+package com.sipe.slack.helping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpingApplication.class, args);
+    }
+
+}
---
+++ helping/src/test/java/com/sipe/slack/helping/HelpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipe.slack.helping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
